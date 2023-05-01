22bf0c657b45701b0edff1136143e30e8d3dbc84
SonPham14 <103044792+SonPham14@users.noreply.github.com>
2023-05-01 22:57:06 +0700

    Update Extract_Data_from_LSB_Steganography.py

--- Task 2/Bài 3/Extract_Data_from_LSB_Steganography.py
+++ Task 2/Bài 3/Extract_Data_from_LSB_Steganography.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 
+#Get a bit from RGB 
 def Extract_data(p, c):
     if c == 'r':
         return bin(p)[-2]
@@ -9,17 +10,19 @@ def Extract_data(p, c):
     else:
         return bin(p)[-3]
 
+#Convert image to RGB
 img = Image.open("D:\\Training_Forensics\\Bai2_LSB.png").convert('RGB')
 data = img.getdata()
-message_bits = ""
 
+#Get binary string
+message_bits = ""
 for pixel in data:
     r, g, b = pixel
     message_bits += Extract_data(r, 'r')
     message_bits += Extract_data(g, 'g')
     message_bits += Extract_data(b, 'b')
 
-
+#Convert binary string to Ascii string
 i = 0
 message = ""
 while message_bits[i:i+8] != "11111111":
